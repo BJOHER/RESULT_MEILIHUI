e933fa142d5ff076d8210a7f28df9b276767d0d8
BJOHER <1274863378@qq.com>
2019-01-11 10:24:12 +0800

    BJH

---
+++ src/views/productdetail/productdetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+class Productdetail extends Component{
+    render(){
+        return (
+            <div>
+                Productdetail
+            </div>
+        )
+    }
+}
+export default Productdetail
